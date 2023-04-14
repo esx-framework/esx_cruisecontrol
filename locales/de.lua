598a99b119aa8c671fbd5d42232552036612bcd6
Lenny822 <84620158+Lenny822@users.noreply.github.com>
2023-04-15 01:56:21 +0200

    Create de.lua

---
+++ locales/de.lua
@@ -0,0 +1,7 @@
+Locales['de'] = { 
+  ['activated']   = 'Fahrkontrolle aktiviert',
+  ['deactivated'] = 'Fahrkontrolle deaktiviert',
+  ['increaseSpeed'] = 'Geschwindigkeit erhöhen',
+  ['decreaseSpeed'] = 'Geschwindigkeit verniedrigeren',
+  ['cruiseControl'] = 'Fahrkontrolle'
+}
