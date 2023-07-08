811a5075f3868b2a731af0d471f47e845f6319e7
Gellipapa <gellerthorvath0@gmail.com>
2023-07-09 00:50:24 +0200

    :memo: Add cs locale

---
+++ locales/cs.lua
@@ -0,0 +1,12 @@
+Locales['cs'] = {
+    ['activated']   = 'Tempomat byl aktivován',
+    ['deactivated'] = 'Tempomat byl deaktivován',
+    ['increaseSpeed'] = 'Zvýšit rychlost',
+    ['decreaseSpeed'] = 'Znížit rychlost',
+    ['cruiseControl'] = 'Tempomat',
+  
+    --Pás
+    ['toggleSeatbelt'] = "Zapnout pás",
+    ["seatbeltOn"] = "Zapásli jste se",
+    ["seatbeltOff"] = "Odopásli jste se"
+  }
