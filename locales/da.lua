59e6cde150393694776cee0c89e71fef68a77c09
Rasmus <109039505+RalleeDev@users.noreply.github.com>
2024-07-16 15:38:36 +0200

    Create da.lua
    Add Danish language

---
+++ locales/da.lua
@@ -0,0 +1,12 @@
+Locales['da'] = {
+  ['activated'] = 'Fartpilot aktiveret',
+  ['deactivated'] = 'Fartpilot deaktiveret',
+  ['increaseSpeed'] = 'Øg Hastighed',
+  ['decreaseSpeed'] = 'Reducer Hastighed',
+  ['cruiseControl'] = 'Fartpilot',
+
+  -- Sikkerhedssele
+  ['toggleSeatbelt'] = 'Skift Sikkerhedssele',
+  ['seatbeltOn'] = 'Sikkerhedssele PÅ',
+  ['seatbeltOff'] = 'Sikkerhedssele FRA'
+}
